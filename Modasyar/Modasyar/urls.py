ad927996cb2e4c829a397284244fa87427013a26
iniyoel <iniyoel12@gmail.com>
2024-06-25 20:15:49 +0700

    last

--- Modasyar/Modasyar/urls.py
+++ Modasyar/Modasyar/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from django.urls import path,include
-from myApp import views
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("myApp.urls")),
+    path('app/', include('myApp.urls')),
+    path('', TemplateView.as_view(template_name='Dashboard.html')),
+    path('accounts/', include('allauth.urls')),
 ]
-
-from django.conf import settings
-from django.conf.urls.static import static
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Modasyar/Modasyar/wsgi.py
+++ Modasyar/Modasyar/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Modasyar project.
+WSGI config for modasyar project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Modasyar.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'modasyar.settings')
 
 application = get_wsgi_application()
